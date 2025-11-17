0c8a6ac5c1da47fa3179f63037145ae2f50ed5ba
vandersonr7 <vandersonrosseti@gmail.com>
2025-11-17 15:21:27 -0300

    Create App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <div style={{ padding: '20px', textAlign: 'center' }}>
+      <h1>Ponto Digital Pro</h1>
+      <p>Sistema de gerenciamento de horas e projetos</p>
+      <p>Carregando...</p>
+    </div>
+  );
+}
